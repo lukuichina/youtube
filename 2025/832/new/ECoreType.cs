de940658f3f87d513f6ddf389f57f0caabc3b9d7
lukuichina <lukuichin@gmail.com>
2025-12-08 07:07:14 +0900

    第832期
    https://youtu.be/espn7wMWsdU

---
+++ 2025/832/new/ECoreType.cs
@@ -0,0 +1,25 @@
+namespace ServiceLib.Enums;
+
+public enum ECoreType
+{
+    v2fly = 1,
+    Xray = 2,
+    v2fly_v5 = 4,
+    mihomo = 13,
+    hysteria = 21,
+    naiveproxy = 22,
+    tuic = 23,
+    sing_box = 24,
+    juicity = 25,
+    hysteria2 = 26,
+    brook = 27,
+    overtls = 28,
+    shadowquic = 29,
+    mieru = 30,
+    gost = 31,
+    chisel = 32,
+    ssh = 33,
+    sshpass = 34,
+    easytier = 35,
+    v2rayN = 99
+}
---
+++ 2025/832/readme.txt
@@ -0,0 +1,2 @@
+add easytier to core list
+(based on official 7.14.12)
